80ca819ea939aaec4c5ec09090c094efb910b4f4
Martin Milius <martin.milius@gmail.com>
2019-08-20 17:08:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,7 +34,12 @@ end
 
 
 def apply_clearance(cart)
-  
+  cart.each do |product, info|
+    if info[:clerance] == true
+      info[:price] = (info[:price] * 0.8).round(2)
+end
+end
+cart
 end
 
 def checkout(cart, coupons)
