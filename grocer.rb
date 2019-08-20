971dc8902d822f9052ec474e80d248cefaa88472
Martin Milius <martin.milius@gmail.com>
2019-08-20 14:57:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,8 +15,8 @@ def apply_coupons(cart, coupons)
   if cart.key?(coupon[:product]) && cart[coupon[:product]][:count] >= coupon[:num]
     apply = coupon[:product] + "W/COUPON"
     cart[coupon[:item]][:count] -= coupon[:num]
-    if (cart.key(apply)==false)
-      cart[apply] = {price:coupon[:count], clearance: cart[coupon[:item]][:clearance], count:1}
+    if (cart.key?(apply)==false)
+      cart[apply] = {price: coupon[:count], clearance: cart[coupon[:item]][:clearance], count:1}
     else
       cart[apply][:count] += 1
     end
