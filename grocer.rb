37dee238d997e09bd7236b293053400d04f6f8ce
Martin Milius <martin.milius@gmail.com>
2019-08-20 15:48:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,23 +11,7 @@ new_cart
 end
 
 def apply_coupons(cart, coupons) 
-  coupons.each do |coupon| 
-    coupon.each do |attribute, value| 
-      name = coupon[:product] 
-      if cart[name] && cart[name][:count] >= coupon[:num] 
-        if cart["#{name} W/COUPON"] 
-          cart["#{name} W/COUPON"][:count] += 1 
-        else 
-          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
-          :clearance => cart[name][:clearance], :count => 1} 
-        end 
   
-      cart[name][:count] -= coupon[:num] 
-    end 
-  end 
-end 
-  cart 
-end
  
 
  
