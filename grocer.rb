9cfba65695f41a587d8b08c059f30b9e4d7af12c
Martin Milius <martin.milius@gmail.com>
2019-08-20 15:00:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
   if cart.key?(coupon[:product]) && cart[coupon[:product]][:count] >= coupon[:num]
     apply = coupon[:product] + "W/COUPON"
-    cart[coupon[:item]][:count] -= coupon[:num]
+    cart[coupon[:product]][:count] -= coupon[:num]
     if (cart.key?(apply)==false)
       cart[apply] = {price: coupon[:count], clearance: cart[coupon[:item]][:clearance], count:1}
     else
@@ -27,8 +27,8 @@ end
 
 
 def apply_clearance(cart)
-  cart.each do |item, attribute_hash|
-    if attribute_hash
+  cart.each do |product, item|
+    if info
 end
 
 def checkout(cart, coupons)
