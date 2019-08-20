58030d51b46869084b7cb0b97c884b0891c71417
Martin Milius <martin.milius@gmail.com>
2019-08-20 17:11:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,7 +34,7 @@ end
 def apply_clearance(cart)
   cart.each do |product, info|
     if info[:clerance] == true
-      info[:price] = (info[:price] * 0.8).round(2)
+      info[:price] = (info[:price]*0.8).round(2)
 end
 end
 cart
